acd05b326ce820c5f7a3c3dca715323b51e72aeb
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2024-12-29 02:06:19 +0800

    Configure cors for backend frontend integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.routers.data_router import router as data_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(data_router)
 
