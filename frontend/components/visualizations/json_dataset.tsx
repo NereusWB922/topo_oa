4cce4cdfc8aeda8dc5c817d18b8fb5532c9cd192
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2024-12-29 20:26:58 +0800

    Add range slider for line chart and scatter chart

--- frontend/components/visualizations/json_dataset.tsx
+++ frontend/components/visualizations/json_dataset.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo } from 'react'
+'use client';
+import { Slider } from '@mui/material';
+import React, { useCallback, useMemo, useState } from 'react'
 import { CartesianGrid, Legend, ResponsiveContainer, Scatter, ScatterChart, Tooltip, TooltipProps, XAxis, YAxis } from 'recharts';
 
 const CustomTooltip = ({ payload, active } : TooltipProps<any, any>) => {
@@ -19,6 +21,10 @@ const CustomTooltip = ({ payload, active } : TooltipProps<any, any>) => {
 };
 
 export const JSONDataset = ({ data }: { data: any }) => {
+  const minYear = 2015;
+  const maxYear = 2023;
+  const [range, setRange] = useState([minYear, maxYear]);
+
   const transformedData = useMemo(() => {
     if (data === null || data === undefined) return [];
     return data.companies.map((company: any) => {
@@ -33,16 +39,43 @@ export const JSONDataset = ({ data }: { data: any }) => {
 
   }, [data]);
 
+  const filteredData = useMemo(() => {
+    if (transformedData === null || transformedData === undefined) return [];
+    return transformedData.map((company: any) => ({
+        ...company,
+        employees: company.employees.filter((employee: any) => 
+          employee.hired_date >= new Date(`${range[0]}`).getTime() &&
+          employee.hired_date <= new Date(`${range[1]}`).getTime())
+        })
+    );
+  }, [transformedData, range]);
+
+  const onRangeChange = useCallback((event: any, newValue: number | number[]) => {
+      setRange(newValue as number[]);
+    }, [setRange]);
+
   return (
     <div className='relative min-h-screen flex flex-col max-w-screen-xl mx-auto justify-center items-center w-3/4'>
       <h2 className="text-xl font-bold text-center mb-4">Employee Salary vs. Hiring Date by Company</h2>
+      <div className="flex w-1/4 mx-auto space-x-4">
+        <div className="mb-2 text-lg font-semibold">Range</div>
+        <Slider 
+          value={range}
+          onChange={onRangeChange}
+          min={minYear}
+          max={maxYear}
+          step={1}
+          valueLabelDisplay="auto"
+          marks
+        />
+      </div>
       <ResponsiveContainer width="90%" height={450} className="bg-gray-800 p-5 rounded-lg">
         <ScatterChart>
           <CartesianGrid stroke="#444" strokeDasharray="3 3"/>
           <XAxis dataKey="hired_date" type="number" name="Hired Date" stroke="#ccc" domain={['auto', 'auto']} tickFormatter={(tick) => new Date(tick).toLocaleDateString()} />
           <YAxis dataKey="salary" type="number" name="Salary" stroke="#ccc" />
-          <Scatter data={transformedData[0]?.employees} name={transformedData[0]?.name} fill="#8884d8"/>
-          <Scatter data={transformedData[1]?.employees} name={transformedData[1]?.name}  fill="#82ca9d"/>
+          <Scatter data={filteredData[0]?.employees} name={filteredData[0]?.name} fill="#8884d8"/>
+          <Scatter data={filteredData[1]?.employees} name={filteredData[1]?.name}  fill="#82ca9d"/>
           <Legend wrapperStyle={{ color: '#ccc', bottom: 1 }} style={{ marginTop: 10}}/>
           <Tooltip content={<CustomTooltip />}/>
         </ScatterChart>
--- frontend/components/visualizations/pdf_dataset.tsx
+++ frontend/components/visualizations/pdf_dataset.tsx
@@ -1,7 +1,13 @@
-import React, { useMemo } from 'react';
+'use client';
+import { Slider } from '@mui/material';
+import React, { useCallback, useMemo, useState } from 'react';
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
+const quarters = ["2022 Q1", "2022 Q2", "2022 Q3", "2022 Q4", "2023 Q1", "2023 Q2", "2023 Q3", "2023 Q4", "2024 Q1", "2024 Q2"]
+
 export const PDFDataset = ({ data }: { data: any }) => {
+  const [range, setRange] = useState([0, quarters.length - 1]);
+
   const transformedData = useMemo(() => {
     if (data === null || data === undefined) return [];
     return data.map((item: any) => ({
@@ -10,11 +16,35 @@ export const PDFDataset = ({ data }: { data: any }) => {
     }));
   }, [data])
 
+  const filteredData = useMemo(() => {
+    if (transformedData === null || transformedData === undefined) return [];
+    return transformedData.filter((item: any) => 
+      quarters.indexOf(item.yearQuarter) >= range[0] &&
+      quarters.indexOf(item.yearQuarter) <= range[1])
+  }, [transformedData, range])
+
+  const onRangeChange = useCallback((event: any, newValue: number | number[]) => {
+    setRange(newValue as number[]);
+  }, [setRange]);
+
   return (
     <div className='relative min-h-screen flex flex-col max-w-screen-xl mx-auto justify-center items-center w-3/4'>
       <h2 className="text-xl font-bold text-center mb-4">Quarterly Revenue and Membership Trends (2022-2024)</h2>
+      <div className="flex w-1/4 mx-auto space-x-4">
+        <div className="mb-2 text-lg font-semibold">Range</div>
+        <Slider 
+          value={range}
+          onChange={onRangeChange}
+          min={0}
+          max={quarters.length - 1}
+          step={1}
+          valueLabelDisplay="auto"
+          marks
+          valueLabelFormat={(index) => quarters[index]}
+        />
+      </div>
       <ResponsiveContainer width="90%" height={450} className="bg-gray-800 p-5 rounded-lg">
-        <LineChart data={transformedData}>
+        <LineChart data={filteredData}>
           <CartesianGrid stroke="#444" strokeDasharray="3 3"/>
           <XAxis dataKey="yearQuarter" stroke="#ccc" tickMargin={5} />
           <Legend wrapperStyle={{ color: '#ccc', bottom: 1 }} style={{ marginTop: 10}}/>
